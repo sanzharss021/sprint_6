1d61b1cdfbb7b7c7584fd3e17cdb4a6d680475b9
Sultan, Sanzhar <sanzharsultan021@gmail.com>
2025-01-20 10:28:57 +0500

    second-commit

--- precode.go
+++ precode.go
@@ -44,18 +44,15 @@ var tasks = map[string]Task{
 // Ниже напишите обработчики для каждого эндпоинта
 
 func getTasks(w http.ResponseWriter, r *http.Request) {
-	// сериализуем данные из слайса artists
+
 	resp, err := json.Marshal(tasks)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// в заголовок записываем тип контента, у нас это данные в формате JSON
 	w.Header().Set("Content-Type", "application/json")
-	// так как все успешно, то статус OK
 	w.WriteHeader(http.StatusOK)
-	// записываем сериализованные в JSON данные в тело ответа
 	w.Write(resp)
 }
 
